07c842a41c52a98ee06eeb7f8e63ce95932ae98c
ferasabujaber <34807748+ferasabujaber@users.noreply.github.com>
2020-10-01 20:11:15 +0300

    Add files via upload

---
+++ Vehicles.java
@@ -0,0 +1,103 @@
+package OjtAssessment;
+
+import java.util.ArrayList;
+ 
+
+enum Types {
+    Private,
+    Mcycle,
+    Trailer
+  } 
+  enum Status {
+    Waiting,
+    Inprog,
+    End
+  }
+  enum Actions{
+        R,C,None
+  }
+
+public class Vehicles {
+    int  vnum;
+    Types type;
+    Status status; 
+    ArrayList<Actions> actions = new ArrayList<Actions>();
+    Actions act;
+    Actions act1;
+    Actions R;
+    Actions C;
+    int timer=0;
+   // int timerc=0;
+
+
+public  Vehicles (int vnum,Types type,  ArrayList<Actions> actions) {
+    this.vnum=vnum;
+    this.status=Status.Waiting;
+
+    this.type=type;
+
+
+     this.actions=actions; 
+ }
+ public  Vehicles (int vnum,Types type, Actions ac,Actions ac1) {
+    this.vnum=vnum;
+    this.type=type;
+    this.act=ac;
+    this.act1=ac1;
+    this.status=Status.Waiting;    
+
+  }
+  
+public  Vehicles (int vnum,Types type,  Actions act) {
+    this.vnum=vnum;
+
+    this.type=type;
+    this.status=Status.Waiting;
+
+
+     this.act=act; 
+     this.act1=Actions.None;
+     //this.timer=0;
+ }
+public  Vehicles (String x) {
+
+     
+}
+
+public Types getType() {
+    return type;
+    
+}
+public Status getStatus() {
+    return status;
+}
+public void setTimer(int timer){
+
+    this.timer=timer;
+}
+public int getTimer(){
+
+    return this.timer;
+}
+    
+
+public Actions getActions() {
+    return actions.get(0);
+    
+}
+public void setType(Types type) {
+    this.type=type;
+     
+    
+}
+public void setStatus(Status status) {
+    this.status=status;
+}
+public void setActions(ArrayList<Actions> actions) {
+
+    this.actions=actions;
+
+    
+}
+
+}
